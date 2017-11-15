d97af9e03c552469939ccac1f14486a69bf66d9a
aljazskrinjar <aljaz.skrinjar@gmail.com>
2017-11-15 03:39:47 +0100

    adding a batch kind of works

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,11 +1,23 @@
 class GroupsController < ApplicationController
   def index
     @groups=Group.all.order(:name)
+    @group=Group.new
   end
 
   def show
     @group = Group.find(params[:id])
     @students=@group.students
+
+  end
+
+  def create
+    group_params = params.require(:group).permit(:name,:start_date,:end_date)
+    @group=Group.new(group_params)
+
+    if @group.save
+       redirect_to groups_path
+
+    end
 
   end
 end
