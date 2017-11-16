2c8dd4bab750241bf833b290484cf7d829122b88
aljazskrinjar <aljaz.skrinjar@gmail.com>
2017-11-16 12:34:28 +0100

    added save and next

--- app/controllers/grades_controller.rb
+++ app/controllers/grades_controller.rb
@@ -5,29 +5,19 @@ class GradesController < ApplicationController
   def create
     grade_params = params.require(:grade).permit(:remark,:value,:date, :user_id)
     @grade=@student.grades.create(grade_params)
+    if params[:commit] == 'Save'
+      if @grade.save!
+         redirect_to group_student_path(@student.group.id,@student.id)
 
-    if @grade.save!
-       redirect_to group_student_path(@student.group.id,@student.id)
-
-    end
-
-  end
-
-  def edit
-    @grade = Grade.find(params[:id])
-  end
-
-  def update
-    @grade = Grade.find(params[:id])
-
-    grade_params = params.require(:grade).permit(:remark, :value, :date)
-
-    if @grade.update_attributes(grade_params)
-      redirect_to @grade
+      end
     else
-      render 'edit'
+      if @grade.save!
+         redirect_to groups_path
+      end
     end
+
   end
+
 
 
   private
