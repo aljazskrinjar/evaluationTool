526ccbff55e5431b4f83e8bfb39468bc13730ff8
aljazskrinjar <aljaz.skrinjar@gmail.com>
2017-11-16 17:40:23 +0100

    add student working

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,9 +1,25 @@
 class StudentsController < ApplicationController
+  before_action :set_group
   def show
     @student = Student.find(params[:id])
     @group = @student.group
     @grades = @student.grades
+  end
 
+  def create
+    student_params = params.require(:student).permit(:name,:surname,:photo)
+    @student=@group.students.create(student_params)
 
+    if @group.save
+       redirect_to group_path(@group.id)
+
+    end
+
+  end
+
+  private
+
+  def set_group
+    @group = Group.find(params[:group_id])
   end
 end
